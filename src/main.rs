c5e15cbd39d7df8a69876c1fae98346aaade7f10
Yizhen Shi <yizhenshi@Familys-MacBook-Pro.local>
2024-12-23 17:31:16 -0500

    chapter 2 donw

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use crossterm::terminal::disable_raw_mode;
+use crossterm::terminal::enable_raw_mode;
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+    enable_raw_mode().unwrap();
+    for b in io::stdin().bytes() {
+        match b {
+            Ok(b) => {
+                let c = b as char;
+                if c.is_control() {
+                    println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
+                } else {
+                    println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
+                }
+                if c == 'q' {
+                    break;
+                }
+            }
+            Err(err) => {
+                println!("Error: {}", err);
+            }
+        }
+    }
+    disable_raw_mode().unwrap();
 }
